a645d0e5e1b0c2beb0cd878af7383bd1b96824d8
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-09-06 09:17:40 -0700

    Base of Blog all in one

---
+++ Dayana/Shared/Persistence/Models/Blog/Base/IssueBaseFile.cs
@@ -0,0 +1,37 @@
+﻿using Dayana.Shared.Persistence.Models.Identity.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dayana.Shared.Persistence.Models.Blog.Base;
+public class PostCategoryIssueModel : BaseModel
+{
+    public string IssueTitle { get; set; }
+    public string IssueDescription { get; set; }
+
+    #region Navigation
+    public int PostCategoryId { get; set; }
+    public PostCategoryModel PostCategory { get; set; }
+
+    public int IssueWriterId { get; set; }
+    public UserModel IssueWriter { get; set; }
+    public ICollection<PostCategoryIssueCommentModel> PostCategoryIssueComments { get; set; }
+    #endregion
+}
+
+public class PostIssueModel : BaseModel
+{
+    public string IssueTitle { get; set; }
+    public string IssueDescription { get; set; }
+
+    #region Navigation
+    public int PostId { get; set; }
+    public PostModel Post { get; set; }
+
+    public int IssueWriterId { get; set; }
+    public UserModel IssueWriter { get; set; }
+    public ICollection<PostIssueCommentModel> PostIssueComments { get; set; }
+    #endregion
+}
