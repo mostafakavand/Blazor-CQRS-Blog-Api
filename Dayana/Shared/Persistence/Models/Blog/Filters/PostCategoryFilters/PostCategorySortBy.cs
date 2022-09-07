b7268d381ab35a38d04fd46d3b4e28ff32d89f05
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2022-09-07 04:16:41 -0700

    post and post category filters all in one

--- Dayana/Shared/Persistence/Models/Blog/Filters/PostCategoryFilters/PostCategorySortBy.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Dayana.Shared.Persistence.Models.Blog.Filters.BlogPosts.PostCategoryFilters;
-
-public enum PostCategorySortBy
-{
-    CreationDate = 12,
-    CreationDateDescending = 13,
-}
---
+++ Dayana/Shared/Persistence/Models/Blog/Filters/PostFiltersFile.cs
@@ -0,0 +1,27 @@
+﻿using Dayana.Shared.Infrastructure.Pagination;
+using Dayana.Shared.Persistence.Models.Blog.Filters.BlogPosts.PostFilters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dayana.Shared.Persistence.Models.Blog.Filters;
+
+public class PostFilter : PaginationFilter
+{
+    public PostFilter(int page, int pageSize) : base(page, pageSize)
+    {
+
+    }
+
+    public string KeyWord { get; set; }
+
+    public PostSortBy? SortBy { get; set; }
+}
+
+public enum PostSortBy
+{
+    CreationDate = 12,
+    CreationDateDescending = 13,
+}
