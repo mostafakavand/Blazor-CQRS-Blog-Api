0b2e5df56dc6531732d4687a1138dd1f566d8822
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2022-09-28 10:20:02 +0330

    blog commands done

--- Dayana/Shared/Persistence/Models/Blog/Commands/PostCommandsFile.cs
+++ Dayana/Shared/Persistence/Models/Blog/Commands/PostCommandsFile.cs
@@ -42,3 +42,37 @@ public class UpdatePostCommand : IRequestInfo, IRequest<OperationResult>
     public string Summery { get; set; }
     public string TextContent { get; set; }
 }
+public class CreatePostCategoryCommand : IRequestInfo, IRequest<OperationResult>
+{
+    public CreatePostCategoryCommand(RequestInfo requestInfo)
+    {
+        RequestInfo = requestInfo;
+    }
+    public string CategoryTitle { get; set; }
+    public string CategoryIcon { get; set; }
+
+    public RequestInfo RequestInfo { get; set; }
+}
+
+public class DeletePostCategoryCommand : IRequestInfo, IRequest<OperationResult>
+{
+    public DeletePostCategoryCommand(RequestInfo requestInfo)
+    {
+        RequestInfo = requestInfo;
+    }
+    public int Id { get; set; }
+    public RequestInfo RequestInfo { get; set; }
+}
+
+public class UpdatePostCategoryCommand : IRequestInfo, IRequest<OperationResult>
+{
+    public UpdatePostCategoryCommand(RequestInfo requestInfo)
+    {
+        RequestInfo = requestInfo;
+    }
+    public int Id { get; set; }
+    public string CategoryTitle { get; set; }
+    public string CategoryIcon { get; set; }
+
+    public RequestInfo RequestInfo { get; set; }
+}
