49e94ef0d284936b91787f1c229adf041c4d4ba6
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2022-10-01 10:51:35 +0330

    0

--- Dayana/Shared/Infrastructure/Errors/Blog/PostCategoryErrors.cs
+++ Dayana/Shared/Infrastructure/Errors/Blog/PostCategoryErrors.cs
@@ -1,4 +1,29 @@
-﻿namespace Dayana.Shared.Infrastructure.Errors.Blog;
-internal class PostCategoryErrors
+﻿using Dayana.Shared.Basic.ConfigAndConstants.Constants;
+
+namespace Dayana.Shared.Infrastructure.Errors.Blog;
+public static class PostCategoryErrors
 {
+    public static ErrorModel InvalidPostCategorydError = new ErrorModel(
+       code: 600,
+       title: "Blog Error",
+       (
+           Language: Language.English,
+           Message: "Invalid Post Category  Id"
+       ));
+
+    public static ErrorModel DuplicatePostCategoryCommentContextError = new ErrorModel(
+        code: 601,
+        title: "Blog Error",
+        (
+            Language: Language.English,
+            Message: "Post Category Comment With This Context Exists"
+        ));
+
+    public static ErrorModel DuplicatePostCategoryCommentContextError = new ErrorModel(
+       code: 601,
+       title: "Blog Error",
+       (
+           Language: Language.English,
+           Message: "Post Category Comment With This Context Exists"
+       ));
 }
