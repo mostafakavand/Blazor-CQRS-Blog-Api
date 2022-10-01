c51de575507fba2733c85b8a135e6467fc9d6093
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2022-10-01 11:56:41 +0330

    ge

---
+++ Dayana/Shared/Infrastructure/Errors/GenericErrors.cs
@@ -0,0 +1,37 @@
+﻿using Dayana.Shared.Basic.ConfigAndConstants.Constants;
+
+namespace Dayana.Shared.Infrastructure.Errors;
+public static class GenericErrors<T>
+{
+    public static ErrorModel InvalidIdError = new ErrorModel(
+      code: 100,
+      title: "Identity Error",
+      (
+          Language: Language.English,
+          Message: $"Invalid {nameof(T)} id error"
+      ));
+
+    public static ErrorModel InvalidNameError = new ErrorModel(
+     code: 101,
+     title: "Identity Error",
+     (
+         Language: Language.English,
+         Message: $"Invalid {nameof(T)} name error"
+     ));
+
+    public static ErrorModel InvalidTitleError = new ErrorModel(
+     code: 102,
+     title: "Identity Error",
+        (
+       Language: Language.English,
+       Message: $"Invalid {nameof(T)} title error"
+     ));
+
+    public static ErrorModel InvalidVariableError(string variableName) => new ErrorModel(
+     code: 102,
+     title: "Identity Error",
+        (
+       Language: Language.English,
+       Message: $"Invalid property :{variableName} in {nameof(T)} error"
+     ));
+}
