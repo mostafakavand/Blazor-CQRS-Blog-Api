38ebccead6c3f7f5c9f2eaf23bd078340bf10a61
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2022-10-25 01:14:27 -0700

    cemment and issue result filter

---
+++ Dayana/Server/Api/ResultFilters/Blog/CommentResultFilterFile.cs
@@ -0,0 +1,229 @@
+﻿using Dayana.Shared.Domains.Blog.Comments;
+using Dayana.Shared.Infrastructure.Pagination;
+using Dayana.Shared.Persistence.Models.Blog.Base;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Dayana.Shared.Basic.ConfigAndConstants.Constants.ConstMethods;
+
+namespace Dayana.Server.Api.ResultFilters.Blog;
+
+#region post Comment
+
+
+public class CreatePostCommentResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PostComment value)
+            result.Value = new
+            {
+                Eid = value.Id.EncodeInt(),
+                EPostId = value.PostId.EncodeInt(),
+                ECommentOwnerId = value.CommentOwnerId.EncodeInt(),
+                EReplyTOCommentId = value.ReplyToCommentId?.EncodeInt(),
+                value.CommentText,
+                value.CreatedAt,
+                value.UpdatedAt,
+            };
+
+        await next();
+    }
+}
+
+public class DeletePostCommentResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PostComment value)
+            result.Value = new
+            {
+                Eid = value.Id.EncodeInt(),
+                EPostId = value.PostId.EncodeInt(),
+            };
+
+        await next();
+    }
+}
+
+public class GetPostCommentByFilterResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PaginatedList<PostCommentModel> value)
+            result.Value = new
+            {
+                value.Page,
+                value.PageSize,
+                value.TotalCount,
+                Data = value.Data.Select(x => new
+                {
+                    Eid = x.Id.EncodeInt(),
+                    EPostId = x.PostId.EncodeInt(),
+                    ECommentOwnerId = x.CommentOwnerId.EncodeInt(),
+                    EReplyTOCommentId = x.ReplyToCommentId?.EncodeInt(),
+                    x.CommentText,
+                    x.CreatedAt,
+                    x.UpdatedAt,
+                })
+            };
+
+        await next();
+    }
+}
+
+public class GetPostCommentByIdResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PostCommentModel value)
+            result.Value = new
+            {
+                Eid = value.Id.EncodeInt(),
+                EPostId = value.PostId.EncodeInt(),
+                ECommentOwnerId = value.CommentOwnerId.EncodeInt(),
+                EReplyTOCommentId = value.ReplyToCommentId?.EncodeInt(),
+                value.CommentText,
+                value.CreatedAt,
+                value.UpdatedAt,
+            };
+
+        await next();
+    }
+}
+
+public class UpdatePostCommentResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PostComment value)
+            result.Value = new
+            {
+                Eid = value.Id.EncodeInt(),
+                EPostId = value.PostId.EncodeInt(),
+            };
+
+        await next();
+    }
+}
+
+#endregion
+
+#region post category Comment
+
+public class CreatePostCategoryCommentResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PostCategoryComment value)
+            result.Value = new
+            {
+                Eid = value.Id.EncodeInt(),
+                EPostCategoryId = value.PostCategoryId.EncodeInt(),
+                ECommentOwnerId = value.CommentOwnerId.EncodeInt(),
+                EReplyTOCommentId = value.ReplyToCommentId?.EncodeInt(),
+                value.CommentText,
+                value.CreatedAt,
+                value.UpdatedAt,
+            };
+
+        await next();
+    }
+}
+
+public class DeletePostCategoryCommentResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PostCategoryComment value)
+            result.Value = new
+            {
+                Eid = value.Id.EncodeInt(),
+                EPostCategoryId = value.PostCategoryId.EncodeInt(),
+            };
+
+        await next();
+    }
+}
+
+public class GetPostCategoryCommentByFilterResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PaginatedList<PostCategoryCommentModel> value)
+            result.Value = new
+            {
+                value.Page,
+                value.PageSize,
+                value.TotalCount,
+                Data = value.Data.Select(x => new
+                {
+                    Eid = x.Id.EncodeInt(),
+                    EPostCategoryId = x.PostCategoryId.EncodeInt(),
+                    ECommentOwnerId = x.CommentOwnerId.EncodeInt(),
+                    EReplyTOCommentId = x.ReplyToCommentId?.EncodeInt(),
+                    x.CommentText,
+                    x.CreatedAt,
+                    x.UpdatedAt,
+                })
+            };
+
+        await next();
+    }
+}
+
+public class GetPostCategoryCommentByIdResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PostCategoryCommentModel value)
+            result.Value = new
+            {
+                Eid = value.Id.EncodeInt(),
+                EPostCategoryId = value.PostCategoryId.EncodeInt(),
+                ECommentOwnerId = value.CommentOwnerId.EncodeInt(),
+                EReplyTOCommentId = value.ReplyToCommentId?.EncodeInt(),
+                value.CommentText,
+                value.CreatedAt,
+                value.UpdatedAt,
+            };
+
+        await next();
+    }
+}
+
+public class UpdatePostCategoryCommentResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PostCategoryComment value)
+            result.Value = new
+            {
+                Eid = value.Id.EncodeInt(),
+                EPostCategoryId = value.PostCategoryId.EncodeInt(),
+            };
+
+        await next();
+    }
+}
+
+#endregion
--- Dayana/Server/Api/ResultFilters/Blog/IssueResultFilterFile.cs
+++ Dayana/Server/Api/ResultFilters/Blog/IssueResultFilterFile.cs
@@ -5,8 +5,231 @@ using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc;
 using Dayana.Shared.Domains.Blog.Issues;
 using Dayana.Shared.Basic.ConfigAndConstants.Constants.ConstMethods;
+using Dayana.Shared.Domains.Blog.Comments;
 
 namespace Dayana.Server.Api.ResultFilters.Blog;
+
+#region post issue Comment
+
+
+public class CreatePostIssueCommentResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PostIssueComment value)
+            result.Value = new
+            {
+                Eid = value.Id.EncodeInt(),
+                EPostIssueId = value.PostIssueId.EncodeInt(),
+                ECommentOwnerId = value.CommentOwnerId.EncodeInt(),
+                EReplyTOCommentId = value.ReplyToCommentId?.EncodeInt(),
+                value.CommentText,
+                value.CreatedAt,
+                value.UpdatedAt,
+            };  
+
+        await next();
+    }
+}
+
+public class DeletePostIssueCommentResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PostIssueComment value)
+            result.Value = new
+            {
+                Eid = value.Id.EncodeInt(),
+                EPostIssueId = value.PostIssueId.EncodeInt(),
+            };
+
+        await next();
+    }
+}
+
+public class GetPostIssueCommentByFilterResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PaginatedList<PostIssueCommentModel> value)
+            result.Value = new
+            {
+                value.Page,
+                value.PageSize,
+                value.TotalCount,
+                Data = value.Data.Select(x => new
+                {
+                    Eid = x.Id.EncodeInt(),
+                    EPostIssueId = x.PostIssueId.EncodeInt(),
+                    ECommentOwnerId = x.CommentOwnerId.EncodeInt(),
+                    EReplyTOCommentId = x.ReplyToCommentId?.EncodeInt(),
+                    x.CommentText,
+                    x.CreatedAt,
+                    x.UpdatedAt,
+                })
+            };
+
+        await next();
+    }
+}
+
+public class GetPostIssueCommentByIdResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PostIssueCommentModel value)
+            result.Value = new
+            {
+                Eid = value.Id.EncodeInt(),
+                EPostIssueId = value.PostIssueId.EncodeInt(),
+                ECommentOwnerId = value.CommentOwnerId.EncodeInt(),
+                EReplyTOCommentId = value.ReplyToCommentId?.EncodeInt(),
+                value.CommentText,
+                value.CreatedAt,
+                value.UpdatedAt,
+            };
+
+        await next();
+    }
+}
+
+public class UpdatePostIssueCommentResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PostIssueComment value)
+            result.Value = new
+            {
+                Eid = value.Id.EncodeInt(),
+                EPostIssueId = value.PostIssueId.EncodeInt(),
+            };
+
+        await next();
+    }
+}
+
+#endregion
+
+#region post category issue Comment
+
+
+public class CreatePostCategoryIssueCommentResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PostCategoryIssueComment value)
+            result.Value = new
+            {
+                Eid = value.Id.EncodeInt(),
+                EPostCategoryIssueId = value.PostCategoryIssueId.EncodeInt(),
+                ECommentOwnerId = value.CommentOwnerId.EncodeInt(),
+                EReplyTOCommentId = value.ReplyToCommentId?.EncodeInt(),
+                value.CommentText,
+                value.CreatedAt,
+                value.UpdatedAt,
+            };
+
+        await next();
+    }
+}
+
+public class DeletePostCategoryIssueCommentResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PostCategoryIssueComment value)
+            result.Value = new
+            {
+                Eid = value.Id.EncodeInt(),
+                EPostCAtegoryIssueId = value.PostCategoryIssueId.EncodeInt(),
+            };
+
+        await next();
+    }
+}
+
+public class GetPostCategoryIssueCommentByFilterResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PaginatedList<PostCategoryIssueCommentModel> value)
+            result.Value = new
+            {
+                value.Page,
+                value.PageSize,
+                value.TotalCount,
+                Data = value.Data.Select(x => new
+                {
+                    Eid = x.Id.EncodeInt(),
+                    EPostCategoryIssueId = x.PostCategoryIssueId.EncodeInt(),
+                    ECommentOwnerId = x.CommentOwnerId.EncodeInt(),
+                    EReplyTOCommentId = x.ReplyToCommentId?.EncodeInt(),
+                    x.CommentText,
+                    x.CreatedAt,
+                    x.UpdatedAt,
+                })
+            };
+
+        await next();
+    }
+}
+
+public class GetPostCategoryIssueCommentByIdResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PostCategoryIssueCommentModel value)
+            result.Value = new
+            {
+                Eid = value.Id.EncodeInt(),
+                EPostCategoryIssueId = value.PostCategoryIssueId.EncodeInt(),
+                ECommentOwnerId = value.CommentOwnerId.EncodeInt(),
+                EReplyTOCommentId = value.ReplyToCommentId?.EncodeInt(),
+                value.CommentText,
+                value.CreatedAt,
+                value.UpdatedAt,
+            };
+
+        await next();
+    }
+}
+
+public class UpdatePostCategoryIssueCommentResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PostCategoryIssueComment value)
+            result.Value = new
+            {
+                Eid = value.Id.EncodeInt(),
+                EPostCategoryIssueId = value.PostCategoryIssueId.EncodeInt(),
+            };
+
+        await next();
+    }
+}
+
+#endregion
 
 #region post issue
 
@@ -108,6 +331,114 @@ public class UpdatePostIssueResultFilter : ResultFilterAttribute
             {
                 Eid = value.Id.EncodeInt(),
                 EPostId = value.PostId.EncodeInt(),
+            };
+
+        await next();
+    }
+}
+
+#endregion
+
+#region post category issue
+
+
+public class CreatePostCategoryIssueResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PostCategoryIssue value)
+            result.Value = new
+            {
+                Eid = value.Id.EncodeInt(),
+                EPostCategoryId = value.PostCategoryId.EncodeInt(),
+                EWriterId = value.IssueWriterId.EncodeInt(),
+                value.IssueDescription,
+                value.IssueTitle,
+                value.PostCategoryIssueComments,
+            };
+
+        await next();
+    }
+}
+
+public class DeletePostCategoryIssueResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PostIssue value)
+            result.Value = new
+            {
+                Eid = value.Id.EncodeInt(),
+                EPostId = value.PostId.EncodeInt(),
+            };
+
+        await next();
+    }
+}
+
+public class GetPostCategoryIssueByFilterResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PaginatedList<PostCategoryIssueModel> value)
+            result.Value = new
+            {
+                value.Page,
+                value.PageSize,
+                value.TotalCount,
+                Data = value.Data.Select(x => new
+                {
+                    Eid = x.Id.EncodeInt(),
+                    EPostId = x.PostCategoryId.EncodeInt(),
+                    x.IssueDescription,
+                    x.IssueTitle,
+                    x.PostCategoryIssueComments
+
+                })
+            };
+
+        await next();
+    }
+}
+
+
+public class GetPostCategoryIssueByIdResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PostCategoryIssueModel value)
+            result.Value = new
+            {
+                Eid = value.Id.EncodeInt(),
+                EPostId = value.PostCategoryId.EncodeInt(),
+                value.IssueDescription,
+                value.IssueTitle,
+                value.PostCategoryIssueComments
+            };
+
+        await next();
+    }
+}
+
+public class UpdatePostCategoryIssueResultFilter : ResultFilterAttribute
+{
+    public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var result = context.Result as ObjectResult;
+
+        if (result?.Value is PostCategoryIssue value)
+            result.Value = new
+            {
+                Eid = value.Id.EncodeInt(),
+                EPostId = value.PostCategoryId.EncodeInt(),
             };
 
         await next();
