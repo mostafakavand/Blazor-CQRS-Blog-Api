b12c083819c85e23b11278b3956b492b87ea2bba
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2022-12-26 08:42:04 -0800

    nemidaanam

--- Dayana/Shared/Infrastructure/Pagination/PaginatedList.cs
+++ Dayana/Shared/Infrastructure/Pagination/PaginatedList.cs
@@ -5,4 +5,9 @@ public class PaginatedList<T>
     public int PageSize { get; set; }
     public int TotalCount { get; set; }
     public IList<T> Data { get; set; }
+
+    public int CurrentPage { get; set; }
+    public int TotalPages { get; set; }
+    public bool HasPrevious => CurrentPage > 1;
+    public bool HasNext => CurrentPage < TotalPages;
 }
