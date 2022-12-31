917fb46f377700bbaa8d14e2625401e5e29ea4ee
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2022-12-31 07:44:10 -0800

    fuck youu

--- Dayana/Client/Program.cs
+++ Dayana/Client/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<IHttpService, HttpService>();
+builder.Services.AddScoped<IHttpService,HttpService>();
 
 await builder.Build().RunAsync();
